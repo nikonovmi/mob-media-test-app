56d912c622862849ac1f4241395d026ab434d70e
Michael <nikonof.m.i@gmail.com>
2018-05-16 02:28:19 +0300

    Added two games, implemented saving activity state.

--- MobMediaTestApp/app/src/main/java/app/test/mobmediatest/MainActivity.java
+++ MobMediaTestApp/app/src/main/java/app/test/mobmediatest/MainActivity.java
@@ -9,16 +9,23 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.webkit.URLUtil;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String SAVED_STATE_KEY = "saved_state";
 
-    DrawerLayout mDrawer;
+    private DrawerLayout mDrawer;
+    private WebView mWebView;
+    private State mCurrentState = State.HOME;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -30,6 +37,13 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        mWebView = findViewById(R.id.web_view);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setAllowFileAccess(true);
+        mWebView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.setWebViewClient(new GameWebViewClient());
     }
 
     @Override
@@ -37,12 +51,79 @@ public class MainActivity extends AppCompatActivity
         if (mDrawer.isDrawerOpen(GravityCompat.START)) {
             mDrawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            if (mCurrentState != State.HOME) {
+                mCurrentState = State.HOME;
+                updateWebView();
+            } else {
+                super.onBackPressed();
+            }
         }
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        updateWebView();
+    }
+
+    @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+        switch (item.getItemId()) {
+            case R.id.nav_main_screen:
+                mCurrentState = State.HOME;
+                break;
+            case R.id.nav_game_1:
+                mCurrentState = State.GAME_1;
+                break;
+            case R.id.nav_game_2:
+                mCurrentState = State.GAME_2;
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown navigation item");
+        }
+        updateWebView();
+        mDrawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putSerializable(SAVED_STATE_KEY, mCurrentState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mCurrentState = (State) savedInstanceState.getSerializable(SAVED_STATE_KEY);
+    }
+
+    private void updateWebView() {
+        switch (mCurrentState) {
+            case HOME:
+                mWebView.reload();
+                mWebView.loadUrl("file:///android_asset/home/home.html");
+                break;
+            case GAME_1:
+                mWebView.loadUrl("file:///android_asset/game1/index.html");
+                break;
+            case GAME_2:
+                mWebView.loadUrl("file:///android_asset/game2/index.html");
+                break;
+        }
+    }
+
+    private enum State {
+        HOME, GAME_1, GAME_2
+    }
+
+    private class GameWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (!URLUtil.isNetworkUrl(url)) {
+                return true;
+            }
+            return super.shouldOverrideUrlLoading(view, url);
+        }
     }
 }
